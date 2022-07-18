2e7a0a5f826b4a607d009b8d603fc7788838610f
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-07-18 14:36:43 +0400

    :lipstick: Updates Hero Text

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -12,11 +12,12 @@ export const Hero = (): h.JSX.Element => {
       <div
         class={tw`absolute inset-x-0 -inset-y-64 bg-opacity-90 bg-gradient-to-t from-transparent via-gray-50 to-transparent dark:via-gray-900`}></div>
       <h1
-        class={tw`z-10 mx-auto max-w-screen-sm bg-gradient-to-tl from-blue-700 to-blue-400 bg-clip-text text-6xl tracking-wide dark:from-blue-500 dark:to-blue-200 md:text-center`}>
-        Eric Kwoka is a
-        <span class={tw`font-semibold text-transparent`}> Full-Stack </span>
+        class={tw`z-10 mx-auto max-w-screen-sm bg-gradient-to-tl from-blue-700 to-blue-400 bg-clip-text text-6xl leading-snug tracking-wide dark:from-blue-500 dark:to-blue-200 md:text-center`}>
+        Hi, I'm
+        <span class={tw`font-semibold text-transparent`}> Eric Kwoka</span>. I
+        am a<span class={tw`font-semibold text-transparent`}> Full-Stack </span>
         Engineer and User Experience
-        <span class={tw`font-semibold text-transparent`}> Professional </span>.
+        <span class={tw`font-semibold text-transparent`}> Professional </span>
       </h1>
     </section>
   );
