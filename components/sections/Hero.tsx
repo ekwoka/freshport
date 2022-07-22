36375fac0472cad7cbe37b2079e50686e00cc88c
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-07-22 15:14:20 +0400

    :sparkles: Adds Footer

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -12,7 +12,7 @@ export const Hero = (): h.JSX.Element => {
       <div
         class={tw`absolute inset-x-0 -inset-y-64 bg-opacity-90 bg-gradient-to-t from-transparent via-gray-50 to-transparent dark:via-gray-900`}></div>
       <h1
-        class={tw`z-10 mx-auto max-w-screen-sm bg-gradient-to-tl from-blue-700 to-blue-400 bg-clip-text text-6xl leading-snug tracking-wide dark:from-blue-500 dark:to-blue-200 md:text-center`}>
+        class={tw`z-10 mx-auto max-w-screen-sm bg-gradient-to-tl from-blue-700 to-blue-400 bg-clip-text text-4xl leading-snug tracking-wide dark:from-blue-500 dark:to-blue-200 md:text-center md:text-6xl`}>
         Hi, I'm
         <span class={tw`font-semibold text-transparent`}> Eric Kwoka</span>. I
         am a<span class={tw`font-semibold text-transparent`}> Full-Stack </span>
