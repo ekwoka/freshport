e09d8bc786fdf43eb720ff2ab71f9724172a090f
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-10-01 16:10:15 +0400

    :lipstick: Updates large screen navigation

--- components/molecules/Navigation.tsx
+++ components/molecules/Navigation.tsx
@@ -7,21 +7,19 @@ const pages = [
 export const Navigation = () => {
   return (
     <nav
-      class="fixed bottom-6 z-20 flex w-full justify-center md:bottom-auto md:top-2"
+      class="fixed bottom-6 z-20 flex lg:flex-col w-full justify-center md:bottom-auto md:top-2 lg:left-8 lg:min-h-screen"
       aria-label="Breadcrumb">
       <ol
         role="list"
-        class="flex gap-2 rounded-md bg-gray-50 px-6 shadow dark:bg-gray-800 ">
+        class="flex gap-2 rounded-md bg-gray-50 shadow dark:bg-gray-800 lg:flex-col lg:max-w-min overflow-hidden px-2 md:px-0">
         {pages.map((page) => (
-          <li key={page.name} class="flex">
-            <div class="flex items-center">
-              <a
-                href={page.href}
-                class="px-2 py-4 text-sm font-medium text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100"
-                aria-current={undefined}>
-                {page.name}
-              </a>
-            </div>
+          <li key={page.name} class="flex items-center hover:bg-white hover:bg-opacity-10">
+            <a
+              href={page.href}
+              class="px-2 md:px-8 py-4 text-sm font-medium text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100 w-full"
+              aria-current={undefined}>
+              {page.name}
+            </a>
           </li>
         ))}
       </ol>
