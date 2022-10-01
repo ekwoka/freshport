608281ac74f5e1c2401a28c588343f9b22a5eff2
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-10-01 16:17:34 +0400

    :bug: Fixes navigation pointer events

--- components/molecules/Navigation.tsx
+++ components/molecules/Navigation.tsx
@@ -7,11 +7,11 @@ const pages = [
 export const Navigation = () => {
   return (
     <nav
-      class="fixed bottom-6 z-20 flex lg:flex-col w-full justify-center md:bottom-auto md:top-2 lg:left-8 lg:min-h-screen"
+      class="fixed bottom-6 z-20 flex lg:flex-col w-full justify-center md:bottom-auto md:top-2 lg:left-8 lg:min-h-screen pointer-events-none"
       aria-label="Breadcrumb">
       <ol
         role="list"
-        class="flex gap-2 rounded-md bg-gray-50 shadow dark:bg-gray-800 lg:flex-col lg:max-w-min overflow-hidden px-2 md:px-0">
+        class="flex gap-2 rounded-md bg-gray-50 shadow dark:bg-gray-800 lg:flex-col lg:max-w-min overflow-hidden px-2 md:px-0 pointer-events-auto">
         {pages.map((page) => (
           <li key={page.name} class="flex items-center hover:bg-white hover:bg-opacity-10">
             <a
