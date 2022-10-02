b18c4164db3eeaaf04a613242b28369d358cfae6
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-10-02 16:41:45 +0400

    :poop: Adds function for fetching readme

---
+++ utils/getReadme.ts
@@ -0,0 +1,21 @@
+export const getReadme = async (repo: string) => {
+  const readme = await Promise.any(
+    branches.map(
+      branch => readmeNames.map(
+        name => fetchReadme(repo, branch, name)
+      )
+    ).flat()
+  )
+  return readme
+}
+
+const fetchReadme = async (repo: string, branch: string, name: string) => {
+  const url = `https://raw.githubusercontent.com/${repo}/${branch}/${name}`
+  const response = await fetch(url)
+  if (!response.ok) throw new Error('Not Found')
+  return response.text()
+}
+
+const branches = ['main','master']
+
+const readmeNames = ['README.md','readme.md']
