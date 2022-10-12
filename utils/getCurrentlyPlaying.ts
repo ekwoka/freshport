10185ebf2d30f72a9dd32eee688cc72493303e9b
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-10-12 22:13:20 +0400

    :mute: Removes unneeded logs

--- utils/getCurrentlyPlaying.ts
+++ utils/getCurrentlyPlaying.ts
@@ -7,21 +7,26 @@ import {
 
 let currentTrack: Track | null = null;
 
+let token: string | null = null;
+
 export const getCurrentlyPlaying = async () => {
   if (currentTrack) return currentTrack;
   try {
-    console.log('refetching track');
-    // deno-lint-ignore no-explicit-any
-    (globalThis as any).process = {
-      env: {
-        SPOTIFY_CLIENT: Deno.env.get('SPOTIFY_CLIENT'),
-        SPOTIFY_SECRET: Deno.env.get('SPOTIFY_SECRET'),
-      },
-    };
-    const { access_token } = await refreshToken(
-      Deno.env.get('REFRESH_TOKEN') as string
-    );
-    const client = spotifyApiClient(access_token);
+    if (!token) {
+      // deno-lint-ignore no-explicit-any
+      (globalThis as any).process = {
+        env: {
+          SPOTIFY_CLIENT: Deno.env.get('SPOTIFY_CLIENT'),
+          SPOTIFY_SECRET: Deno.env.get('SPOTIFY_SECRET'),
+        },
+      };
+      const { access_token } = await refreshToken(
+        Deno.env.get('REFRESH_TOKEN') as string
+      );
+      token = access_token;
+      setTimeout(() => (token = null), 1000 * 60 * 30);
+    }
+    const client = spotifyApiClient(token);
     const { item } = await client(currentlyPlayingTrack());
     currentTrack = item;
     setTimeout(() => (currentTrack = null), 1000 * 60 * 3);
