ac0ffd23c35859dc75976193cda8b413895417b1
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-10-13 20:12:11 +0400

    :lipstick: Updates Currently Listening for mobile

--- components/atoms/CurrentlyPlaying.tsx
+++ components/atoms/CurrentlyPlaying.tsx
@@ -2,7 +2,7 @@ import { Image, Track } from '@ekwoka/spotify-api';
 
 export const CurrentlyPlaying = ({ track }: { track: Track }) =>
   track ? (
-    <div class="item-center fixed right-8 bottom-12 z-10 hidden justify-center overflow-hidden rounded-lg shadow md:flex">
+    <div class="absolute inset-x-8 top-8 z-10 flex items-center justify-center overflow-hidden rounded-lg bg-white bg-opacity-30 shadow backdrop-blur-[2px] backdrop-filter dark:bg-black dark:bg-opacity-30 md:fixed md:top-auto md:left-auto md:right-8 md:bottom-12 md:flex">
       <img
         src={
           (
@@ -10,13 +10,19 @@ export const CurrentlyPlaying = ({ track }: { track: Track }) =>
             track.album.images[0]
           ).url
         }
-        class="h-16 w-16"
+        class="hidden h-16 w-16 md:block"
       />
-      <div class="flex flex-col items-center justify-center bg-white bg-opacity-30 px-4 py-1 leading-tight text-gray-800 backdrop-blur-[2px] backdrop-filter dark:bg-black dark:bg-opacity-30 dark:text-gray-200">
-        <span class="text-sm text-green-600 dark:text-green-400">
+      <div class="flex flex-row items-center justify-center gap-x-4 px-4 py-1 leading-tight text-gray-800 dark:text-gray-200  md:flex-col">
+        <span class="shrink-1 min-w-min grow-0 text-sm text-green-600 dark:text-green-400">
           Currently Listening:
         </span>
-        <span>
+        <span
+          class="grow-1 shrink-0 overflow-hidden"
+          style={{
+            display: '-webkit-box',
+            '-webkit-box-orient': 'vertical',
+            '-webkit-line-clamp': '2',
+          }}>
           {track.name.split(/\s[\(|-]/)[0]} - {track.artists[0].name}
         </span>
       </div>
