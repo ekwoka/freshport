f74f821fffdab9ba4f3d88088bfe74b6c3788082
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-10-13 22:48:20 +0400

    :lipstick: Adjusts alignments

--- components/atoms/CurrentlyPlaying.tsx
+++ components/atoms/CurrentlyPlaying.tsx
@@ -1,8 +1,8 @@
-import { Image, Track } from '@ekwoka/spotify-api';
+import { Track } from '@ekwoka/spotify-api';
 
 export const CurrentlyPlaying = ({ track }: { track: Track }) =>
   track ? (
-    <div class="absolute inset-x-8 top-8 z-10 flex items-center justify-center overflow-hidden rounded-lg border border-gray-300 bg-white bg-opacity-50 shadow backdrop-blur-[2px] backdrop-filter dark:border-gray-800 dark:bg-black dark:bg-opacity-50 md:fixed md:top-auto md:left-auto md:right-8 md:bottom-12 md:flex">
+    <div class="absolute inset-x-8 top-8 z-10 flex items-center justify-center overflow-hidden rounded-lg border border-gray-300 bg-white bg-opacity-50 shadow backdrop-blur-[2px] backdrop-filter dark:border-gray-800 dark:bg-black dark:bg-opacity-50 sm:inset-x-20 md:fixed md:top-auto md:left-auto md:right-8 md:bottom-12 md:flex">
       <img
         src={
           (
@@ -12,12 +12,12 @@ export const CurrentlyPlaying = ({ track }: { track: Track }) =>
         }
         class="hidden h-16 w-16 md:block"
       />
-      <div class="flex flex-row items-center justify-center gap-x-4 px-4 py-1 leading-tight text-gray-800 dark:text-gray-200  md:flex-col">
-        <span class="shrink-1 min-w-min grow-0 text-sm text-green-600 dark:text-green-400">
+      <div class="flex flex-row flex-wrap items-center justify-center gap-x-4 px-4 py-1 leading-tight text-gray-800 dark:text-gray-200  md:flex-col">
+        <span class="shrink-1 min-w-min grow-0 text-center text-sm text-green-600 dark:text-green-400">
           Currently Listening:
         </span>
         <span
-          class="grow-1 shrink-0 overflow-hidden"
+          class="grow-1 shrink-0 overflow-hidden text-center"
           style={{
             display: '-webkit-box',
             '-webkit-box-orient': 'vertical',
