d3148728eebd77ae4fc5263399501be6ba58054e
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-10-22 17:39:08 +0400

    :recycle: Refactors refreshtoken

--- utils/getCurrentlyPlaying.ts
+++ utils/getCurrentlyPlaying.ts
@@ -13,15 +13,10 @@ export const getCurrentlyPlaying = async () => {
   if (currentTrack) return currentTrack;
   try {
     if (!token) {
-      // deno-lint-ignore no-explicit-any
-      (globalThis as any).process = {
-        env: {
-          SPOTIFY_CLIENT: Deno.env.get('SPOTIFY_CLIENT'),
-          SPOTIFY_SECRET: Deno.env.get('SPOTIFY_SECRET'),
-        },
-      };
       const { access_token } = await refreshToken(
-        Deno.env.get('REFRESH_TOKEN') as string
+        Deno.env.get('REFRESH_TOKEN') as string,
+        Deno.env.get('SPOTIFY_CLIENT') as string,
+        Deno.env.get('SPOTIFY_SECRET') as string
       );
       token = access_token;
       setTimeout(() => (token = null), 1000 * 60 * 30);
